d1eee3fabb2c024e8fada927356053b5c50b460b
juacompe <juacompe@gmail.com>
2012-04-28 14:43:08 +0700

    - todo-list shows todos in the store

--- apps/todo/myapp.js
+++ apps/todo/myapp.js
@@ -8,5 +8,48 @@ Ext.onReady(function() {
           ]
     });
 
-    
+    Ext.require('Ext.data.Store');
+    var data = {
+        todos: [
+              { title: 'visit opendream', done: true}
+            , { title: 'train extjs', done: false}
+        ]
+    };
+
+    // Store
+    var store = Ext.create('Ext.data.Store', {
+         autoLoad: true
+         , model: 'Todo'
+         , data: data
+         , proxy: {
+               type: 'memory'
+             , reader: {
+                   type: 'json'
+                 , root: 'todos'
+               }
+           }
+    });
+
+    var todo_list = Ext.get('todo-list');
+
+    var tpl = new Ext.XTemplate(''
+        ,'<tpl for=".">'
+        ,'<li>'
+        ,'<div class="view thumb-wrap">'
+        ,      '<input class="toggle" type="checkbox" {[ values.done ? \'checked="checked"\' : \'\']}>'
+        ,           '<label>{title}</label>'
+        ,            '<a class="destroy"></a>'
+        ,'</div>'
+        ,'</li>'
+        ,'</tpl>'
+    );
+
+    Ext.create('Ext.view.View', {
+        store: store
+        , tpl: tpl
+        , itemSelector: 'div.thumb-wrap'
+        , renderTo: 'todo-list'
+    });
+
+    Ext.get('main').show();
 });
